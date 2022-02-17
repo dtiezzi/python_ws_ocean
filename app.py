1c405137e2ccea54842a06bbf16abb0b7da9dd5d
dtiezzi <dtiezzi@usp.br>
2022-02-16 21:00:29 -0300

    last class

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+from datetime import datetime
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template, flash, request, redirect
 import sqlite3
-
-DATABASE = 'blog.db'
-SECRETKEY = 'pudim'
-
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+load_dotenv()
+SECRETKEY = os.environ.get("SECRETKEY")
+DATABASE = os.environ.get("DATABASE")
 
 def connect_db():
     return sqlite3.connect(DATABASE)
@@ -24,8 +27,19 @@ def req_end(exc):
 def show_msg():
     sql = "SELECT titulo, texto FROM entradas ORDER BY id DESC"
     cur = g.db.execute(sql)
-    msg = []
-    return str(msg)
+    msg = [{'title':title, 'text': text} for title, text in cur.fetchall()]
+    return render_template('showmsg.html', msg=msg)
+
+@app.route("/insert", methods=['POST'])
+def insert():
+    sql = "INSERT INTO entradas(titulo, texto) VALUES (?, ?);"
+    title = request.form['title']
+    text = request.form['text']
+    #date = datetime(now)
+    g.db.execute(sql, [title, text])
+    g.db.commit()
+    g.db.close()
+    return redirect("/")
 
 # if __name__ == "__main__":
 # 	app.run()
--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS entradas;
 CREATE TABLE entradas(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titulo STRING NOT NULL,
-    texto STRING NOT NULL
+    texto STRING NOT NULL,
+    date DATE
 );
